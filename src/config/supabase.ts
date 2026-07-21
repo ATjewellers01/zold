661536ea6ea921fc71ff38bcb6a8b17c05c6701e
ADITYA KUMAR <aadiverma4206@gmail.com>
2026-07-21 16:55:54 +0530

    1

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -1,6 +1,11 @@
 import "dotenv/config";
-
 import { createClient } from "@supabase/supabase-js";
+
+console.log("SUPABASE_URL =", process.env.SUPABASE_URL);
+console.log(
+  "SUPABASE_SERVICE_ROLE_KEY Exists =",
+  !!process.env.SUPABASE_SERVICE_ROLE_KEY
+);
 
 const supabaseUrl = process.env.SUPABASE_URL as string;
 const supabaseApiKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
